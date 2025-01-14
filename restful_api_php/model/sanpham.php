a672eae91d3baaea6d2336afc0c8c6a318819562
Jerry-NT <nguyenminhtan072001@gmail.com>
2025-01-14 20:02:41 +0700

    update

--- restful_api_php/model/sanpham.php
+++ restful_api_php/model/sanpham.php
@@ -34,6 +34,9 @@ class sanpham
     } else {
         return false;
     }
+
+    // -> 
+
 }
 
 }
--- restful_api_php/qlcuahang (1).sql
+++ restful_api_php/qlcuahang (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th1 10, 2025 lúc 01:09 PM
+-- Thời gian đã tạo: Th1 14, 2025 lúc 01:01 PM
 -- Phiên bản máy phục vụ: 8.3.0
 -- Phiên bản PHP: 8.2.18
 
@@ -73,53 +73,6 @@ INSERT INTO `danhgia` (`MaDG`, `MaNgD`, `MaSp`, `NoiDung`, `DiemDanhGia`) VALUES
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `donhang`
---
-
-DROP TABLE IF EXISTS `donhang`;
-CREATE TABLE IF NOT EXISTS `donhang` (
-  `MaDH` varchar(10) NOT NULL,
-  `MaNgD` varchar(10) DEFAULT NULL,
-  `TongTien` decimal(10,0) DEFAULT NULL,
-  `MoTa` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`MaDH`),
-  KEY `MaNgD` (`MaNgD`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Đang đổ dữ liệu cho bảng `donhang`
---
-
-INSERT INTO `donhang` (`MaDH`, `MaNgD`, `TongTien`, `MoTa`) VALUES
-('DH001', 'ND001', 200000, 'Bánh tráng'),
-('DH002', 'ND001', 20000, 'Tóp Mỡ');
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `giohang`
---
-
-DROP TABLE IF EXISTS `giohang`;
-CREATE TABLE IF NOT EXISTS `giohang` (
-  `MaNgD` varchar(10) DEFAULT NULL,
-  `MaSp` varchar(10) DEFAULT NULL,
-  `SLMua` int DEFAULT NULL,
-  `ThanhTien` decimal(10,0) DEFAULT NULL,
-  KEY `MaNgD` (`MaNgD`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Đang đổ dữ liệu cho bảng `giohang`
---
-
-INSERT INTO `giohang` (`MaNgD`, `MaSp`, `SLMua`, `ThanhTien`) VALUES
-('ND001', 'SP001', 2, 10000),
-('ND001', 'SP002', 1, 10000);
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `hoadon`
 --
 
@@ -128,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `hoadon` (
   `MaHD` varchar(10) NOT NULL,
   `MaNgD` varchar(10) DEFAULT NULL,
   `TongTien` decimal(10,0) DEFAULT NULL,
-  `TrangThai` int DEFAULT NULL,
+  `TrangThai` enum('Đã đặt','Đặt hàng thành công','Đang giao','Đã giao','Đã hủy') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'Đã đặt',
   PRIMARY KEY (`MaHD`),
   KEY `MaNgD` (`MaNgD`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -138,8 +91,8 @@ CREATE TABLE IF NOT EXISTS `hoadon` (
 --
 
 INSERT INTO `hoadon` (`MaHD`, `MaNgD`, `TongTien`, `TrangThai`) VALUES
-('HD001', 'ND001', 200000, 1),
-('HD002', 'ND001', 20000, 1);
+('HD001', 'ND001', 200000, 'Đã đặt'),
+('HD002', 'ND001', 20000, 'Đã đặt');
 
 -- --------------------------------------------------------
 
@@ -161,8 +114,8 @@ CREATE TABLE IF NOT EXISTS `loaisp` (
 --
 
 INSERT INTO `loaisp` (`MaLoai`, `TenLoai`, `HinhLoai`, `TrangThai`) VALUES
-('L001', 'Bánh Tráng', '', 1),
-('L002', 'Tóp Mỡ', '', 1);
+('L001', 'Bánh Tráng', 'https://s.pro.vn/i0y9', 1),
+('L002', 'Tóp Mỡ', 'https://s.pro.vn/w8N5', 1);
 
 -- --------------------------------------------------------
 
@@ -173,6 +126,7 @@ INSERT INTO `loaisp` (`MaLoai`, `TenLoai`, `HinhLoai`, `TrangThai`) VALUES
 DROP TABLE IF EXISTS `ngdung`;
 CREATE TABLE IF NOT EXISTS `ngdung` (
   `MaNgD` varchar(10) NOT NULL,
+  `Email` varchar(128) NOT NULL,
   `TenNgD` varchar(30) DEFAULT NULL,
   `SDT` varchar(10) DEFAULT NULL,
   `TKNgD` varchar(10) DEFAULT NULL,
@@ -186,8 +140,11 @@ CREATE TABLE IF NOT EXISTS `ngdung` (
 -- Đang đổ dữ liệu cho bảng `ngdung`
 --
 
-INSERT INTO `ngdung` (`MaNgD`, `TenNgD`, `SDT`, `TKNgD`, `MatKhauNgD`, `TrangThai`, `ChucVu`) VALUES
-('ND00', 'Nguyen Van A', '0123456789', 'userstest', 'e10adc3949ba59abbe56e057f20f883e', 1, 'NguoiDung');
+INSERT INTO `ngdung` (`MaNgD`, `Email`, `TenNgD`, `SDT`, `TKNgD`, `MatKhauNgD`, `TrangThai`, `ChucVu`) VALUES
+('ND00', 'a', 'Nguyen Van A', '0123456789', 'userstest', 'e10adc3949ba59abbe56e057f20f883e', 1, 'NguoiDung'),
+('ND01', 'a', 'tan', '12648174', 'tan01', 'e10adc3949ba59abbe56e057f20f883e', 1, 'NguoiDung'),
+('ND02', 'a', 'thai', '341241', 'thai0121', 'e10adc3949ba59abbe56e057f20f883e', 1, 'NguoiDung'),
+('ND03', 'a', 'HuuVinh', '0335487716', 'kenvinh', '202cb962ac59075b964b07152d234b70', 1, 'NguoiDung');
 
 -- --------------------------------------------------------
 
@@ -213,8 +170,16 @@ CREATE TABLE IF NOT EXISTS `sanpham` (
 --
 
 INSERT INTO `sanpham` (`MaSp`, `MaLoai`, `TenSp`, `HinhSp`, `MoTa`, `DonGia`, `TrangThai`) VALUES
-('SP001', 'L001', 'Bánh Tráng Cuốn', 'https://media.vneconomy.vn/w800/images/upload/2023/10/28/avatar-anime.jpg', 'Thơm ngon khó cưỡng', 20000, 1),
-('SP002', 'L002', 'Tóp Mỡ Mắm Tỏi', 'https://media.vneconomy.vn/w800/images/upload/2023/10/28/avatar-anime.jpg', 'Thơm ngon khó cưỡng', 30000, 1);
+('SP001', 'L001', 'Bánh Tráng Cuốn', 'https://s.net.vn/IofD', 'Thơm ngon khó cưỡng, ra lò bao khói.', 20000, 1),
+('SP002', 'L002', 'Tóp Mỡ Mắm Tỏi', 'https://s.net.vn/swUF', 'Thơm ngon khó cưỡng', 30000, 1),
+('SP008', 'L001', 'Bánh tráng xì ke', 'https://s.net.vn/LXo8', 'Cay xé lưỡi, ngon bá cháy', 15000, 1),
+('SP003', 'L001', 'Bánh Tráng Trộn', 'https://s.net.vn/1NaZ', 'Bánh tráng thơm ngon, có kèm trứng cút béo ngậy tuyệt đối.', 20000, 1),
+('SP004', 'L002', 'Tóp mỡ hành phi', 'https://s.net.vn/koOK', 'Tóp mỡ giòn rụm, nhai rộp rộp kèm với hành phi thơm lừng', 30000, 1),
+('SP005', 'L002', 'Tóp mỡ miếng', 'https://s.net.vn/UGHj', 'Miếng tóp mỡ dày như mỏ của bạn, béo ngậy tuyệt đối.\r\n(khuyến cáo không dùng với nước lạnh)', 50000, 1),
+('SP006', 'L001', 'Bánh tráng nướng mắm ruốc', 'https://s.net.vn/d6bJ', 'Nóng hỏi mới ra lò, thơm ngon.', 15000, 1),
+('SP007', 'L001', 'Bánh tráng phơi sương', 'https://s.net.vn/4rY7', 'Dai ngon, muối ngon tuyệt đối', 20000, 1),
+('SP009', 'L002', 'Tóp mỡ chua ngọt', 'http://surl.li/afobrs', 'Chua chua ngọt ngọt bao dính.', 20000, 1),
+('SP010', 'L002', 'Tóp mỡ giòn', 'http://surl.li/ofiwng', 'Thơm ngon giòn rụm.', 20000, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
